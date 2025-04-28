dfb2d886c291cc8e0a8152c61a4d3162db3c847f
MUHAMMAD WAQAS <141298724+Muhammadwaqas1234@users.noreply.github.com>
2025-04-28 18:20:49 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Store from "./components/Store";
 import PaymentPage from "./components/payment";
-import Layout from "./components/Layout"; 
+import Layout from "./components/Layout";
 import ShoeDetails from "./components/Details";
-
 
 function App() {
   return (
-    <Router>
+    <Router basename="/nike"> {/* Important: add basename for GitHub Pages */}
       <Routes>
-        <Route path="/store" element={<Store />} />
-        <Route path="payment" element={<PaymentPage />} />
-        <Route path="/details" element={<ShoeDetails />} />
-
         <Route path="/" element={<Layout />}>
-          
+          {/* Nested Routes */}
+          <Route path="store" element={<Store />} />
+          <Route path="payment" element={<PaymentPage />} />
+          <Route path="details" element={<ShoeDetails />} />
         </Route>
       </Routes>
     </Router>
