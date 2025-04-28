cac37fdf541081b2e4cf419a4543859f2337923f
MUHAMMAD WAQAS <141298724+Muhammadwaqas1234@users.noreply.github.com>
2025-04-28 18:22:34 +0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,52 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
 import Navbar from "./Navbar";
-import Home from "./Home";
-import About from "./About";
-import Product from "./Product";
-import Shop from "./Shop";
-import Sales from "./Sales";
-import Blog from "./Blog";
-import Contact from "./Contact";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <div id="home">
-                <Home />
-            </div>
-
-            <div id="about">
-                <About />
-            </div>
-
-            <div id="product">
-                <Product />
-            </div>
-
-            <div id="shop">
-                <Shop />
-            </div>
-
-            <div id="sales">
-                <Sales />
-            </div>
-
-            <div id="blog">
-                <Blog />
-            </div>
-
-            <div id="contact">
-                <Contact />
-            </div>
-
-            <Outlet />
-        
-    
-
-
       <Outlet />
     </>
   );
