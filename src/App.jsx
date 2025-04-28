1d081b7ddfa5ce49284d926fc8f5e7c56cea687e
MUHAMMAD WAQAS <141298724+Muhammadwaqas1234@users.noreply.github.com>
2025-04-28 18:23:00 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/Layout";
 import Store from "./components/Store";
 import PaymentPage from "./components/payment";
-import Layout from "./components/Layout";
 import ShoeDetails from "./components/Details";
+import Home from "./components/Home";
+import About from "./components/About";
+import Product from "./components/Product";
+import Shop from "./components/Shop";
+import Sales from "./components/Sales";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <Router basename="/nike"> {/* Important: add basename for GitHub Pages */}
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* Nested Routes */}
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="product" element={<Product />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="sales" element={<Sales />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<Contact />} />
           <Route path="store" element={<Store />} />
           <Route path="payment" element={<PaymentPage />} />
           <Route path="details" element={<ShoeDetails />} />
